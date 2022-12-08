305c179d59f3d1219dad1b64da81cff274c95609
shendrongddv <shendrongddv@gmail.com>
2022-12-09 03:26:17 +0700

    Initial

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,10 +8,12 @@ const navbar = document.querySelector(".navbar");
 
 window.addEventListener("scroll", () => {
   if (this.scrollY > 0) {
-    navbar.classList.remove("py-4");
-    navbar.classList.add("py-3", "bg-base");
+    // navbar.classList.remove("py-4");
+    navbar.classList.replace("py-lg-4", "py-lg-2");
+    navbar.classList.add("bg-black", "shadow");
   } else {
-    navbar.classList.add("py-4");
-    navbar.classList.remove("py-3", "bg-base");
+    // navbar.classList.add("py-4");
+    navbar.classList.replace("py-lg-2", "py-lg-4");
+    navbar.classList.remove("bg-black", "shadow");
   }
 });
